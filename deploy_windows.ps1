caa65aa75b64f1ebc515ff4ed97dd142be293f46
asalomon-cpsc <asalomon@cpsc.gov>
2018-10-01 19:32:04 -0400

    adjusted deploy script to add recursion to immediate child item from top level folder

--- deploy_windows.ps1
+++ deploy_windows.ps1
@@ -3,14 +3,14 @@ Param(
   )
 
   if(!(Test-Path $destDir)){
-    Write-Output "Directory doesn't exist, creating it"
+    Write-host "Directory doesn't exist, creating it"
     new-item -ItemType directory -Name recalls-pwa-app -Path $destDir
     $destDir += "recalls-pwa-app"
   # Write-Output "Exiting build program..."
     [Environment]::Exit(8)
 }
 if(!(Test-Path $destDir)){
-    Write-Output "Bad Directory name please try again using a valid directory path."
+    Write-Host "Bad Directory name please try again using a valid directory path."
   # Write-Output "Exiting build program..."
     [Environment]::Exit(8)
 }
@@ -21,13 +21,13 @@ if(Test-Path $deploydir){
   node build/build.js
   ##Write-Output "destination directory exist, deleting..."
   #rimraf $deploydir
-  Write-Output "destination directory deleted"
-  Write-Output "copying build files to destination directory $deploydir"
-  $files = Get-ChildItem  .\dist  
+  Write-Host "destination directory deleted"
+  Write-Host "copying build files to destination directory $deploydir"
+  $files = Get-ChildItem .\dist
   foreach ($file in $files){
    copy-item $file.FullName -Recurse $deploydir
   }
 
-  Write-Output "deployment complete"
+  Write-host "deployment complete"
   
 }
