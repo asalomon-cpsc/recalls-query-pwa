199356552ac0be5f8781b545759f41676b8cf2a8
asalomon-cpsc <asalomon@cpsc.gov>
2018-11-15 16:48:09 -0500

    set drawer to stateless mode

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" disable-resize-watcher disable-route-watcher fixed light :clipped="$vuetify.breakpoint.lgAndUp"
+    <v-navigation-drawer v-model="drawer" stateless touchless fixed light :clipped="$vuetify.breakpoint.lgAndUp"
       app width="310">
       <search></search>
 
@@ -131,7 +131,6 @@ export default {
     });
     window.addEventListener("beforeinstallprompt", e => {
       // Prevent Chrome 67 and earlier from automatically showing the
-      console.log('beforeinstallprompt event fired')
       e.preventDefault();
 
       // Stash the event so it can be triggered later.
