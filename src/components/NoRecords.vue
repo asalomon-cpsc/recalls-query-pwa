04dab6e65a010801c832599ae58da2b6e9017db8
asalomon-cpsc <asalomon@cpsc.gov>
2018-12-14 14:53:03 -0500

    added user guide

---
+++ src/components/NoRecords.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <v-layout align-center>
+      <v-flex xs12 sm12 md12 lg12 xl12 id="noRecords">
+
+        <v-card >
+       
+
+        <v-card-title primary-title >
+          <div>
+           
+            <h4>Search Suggestions:</h4>
+            <div v-for="(item,index) in searchSuggestions" v-bind:key="index"> 
+                
+               <ul >
+                  <li >{{item}} </li>
+               </ul>
+
+            </div>
+          </div>
+        </v-card-title>
+
+        
+      </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  import {
+    EventBus
+  } from "../eventBus.js";
+  export default {
+    name: "noRecords",
+
+   
+    data: function () {
+      return {
+        noRecordsFoundTitle:"No Recalls Found",
+        searchSuggestions:[
+            'Check your spelling',
+            'Use the filters to narrow the search results',
+            'Try broadening the search date range'
+        ]
+      };
+    }
+  };
+
+</script>
+<style scoped>
+ .centered {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+}
+
+</style>
