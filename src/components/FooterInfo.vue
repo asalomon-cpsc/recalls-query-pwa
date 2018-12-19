8f894392736fa99cac9cc3b9eb08450b7f9317ce
asalomon-cpsc <asalomon@cpsc.gov>
2018-12-19 11:48:19 -0500

    added help button for user guide section selection

--- src/components/FooterInfo.vue
+++ src/components/FooterInfo.vue
@@ -21,13 +21,47 @@
              This is an official website of the United States government.
             </div>
             <div>Version: 1.0.0</div>
+            <div class="text-xs-center">
+    <v-menu offset-y>
+      <v-btn slot="activator" top dark flat value="help">
+            <v-icon  dark color="orange lighten-2">help</v-icon>
+          </v-btn>
+      <v-list>
+        <v-list-tile
+          v-for="(item, index) in items"
+          :key="index"
+          @click="redirectToGuide(item)"
+        >
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+  </div>
           </div>
+
+           
+          
     
 </template>
 
 <script>
 export default {
-  name:"footerInfo"
+  name:"footerInfo",
+  data: () => ({
+      items: [
+        { title: 'Help Guide',url:'/docs'},
+        { title: 'Search Overview',url:'/docs/#search-results-overview' },
+        { title: 'Installation',url:'/docs/#installation' },
+        
+      ]
+    }),
+    methods:{
+     redirectToGuide (item){
+       console.log(item)
+      window.location = item.url
+
+      }
+    }
 }
 </script>
 
