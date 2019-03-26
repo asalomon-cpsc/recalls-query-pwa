144760452f6580206d69c1dbc005d88474f72c2a
asalomon-cpsc <asalomon@cpsc.gov>
2019-03-26 13:02:41 -0400

    no changes

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,8 +2,8 @@
 module.exports = {
   NODE_ENV: '"production"',
   DEBUG_MODE: false,
-  ROOT_RECALLS_API: '"/api/recall/"' , //'"https://pwarecallsapiwrapper.azurewebsites.net/"'
-  ROOT_STATS_API: '"/api/stats/"',
-  ROOT_ERROR_API: '"/api/error/"',
+  ROOT_RECALLS_API: '"https://pwa1-appservice.azurewebsites.net/recall/"' , //'"https://pwarecallsapiwrapper.azurewebsites.net/"'
+  ROOT_STATS_API: '"https://pwa1-appservice.azurewebsites.net/stats/"',
+  ROOT_ERROR_API: '"https://pwa1-appservice.azurewebsites.net/error/"',
   
 }
