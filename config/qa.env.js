39f1027cf8630a31d14cd585aa07cb16c2a5dd66
asalomon-cpsc <asalomon@cpsc.gov>
2019-03-26 17:04:20 -0400

    qa config

---
+++ config/qa.env.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    NODE_ENV: '"QA"',
+    DEBUG_MODE: false,
+    ROOT_RECALLS_API: '"https://pwa1-appservice.azurewebsites.net/recall/"' , 
+    ROOT_STATS_API: '"https://pwa1-appservice.azurewebsites.net/stats/"',
+    ROOT_ERROR_API: '"https://pwa1-appservice.azurewebsites.net/error/"',
+    
+  }
+  
